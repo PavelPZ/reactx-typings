c5f6730e0bf9edad63ec4cc3a51a8ee2e301e87a
Pavel Zika <pzika@langmaster.cz>
2017-12-07 00:49:37 +0100

    v

---
+++ make-symlinks.cmd
@@ -0,0 +1,2 @@
+mklink /d "d:\rw\pavelpz\reactx-mui\src\global-typings" "D:\rw\pavelpz\reactx-typings\src\typings"
+mklink /d "D:\rw\pavelpz\reactx-html\src\global-typings" "D:\rw\pavelpz\reactx-typings\src\typings"
